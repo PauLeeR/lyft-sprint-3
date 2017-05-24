30910b14142271eedf7da4cf460b9e51185ee32e
Paulina Rojas <lindaliel@gmail.com>
2017-05-24 11:36:12 -0400

    validacion de coordenadas no funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,45 @@ becomeDriver.addEventListener("click", function(){
   }
 });
 
+
+//a continuación las funciones de validación de coordenadas
+var equis = document.getElementById("x");
+var yehyeh = document.getElementById("y");
+
+document.getElementById("startBtn").addEventListener("click",function(){
+
+  if(validarCoordenadaX(equis)){
+    if(validarCoordenadaY(yehyeh)){
+      document.getElementsByClass("coord").classList.toggle("display-none");
+      document.getElementById("playGame").classList.toggle("display-in");
+    }
+  }
+});
+
+function validarCoordenadaX(equis){ //NO ME FUNCIONAN ESTAS VALIDACIONES, LAS ODIO XD
+    var coordenada = /^[0-9]$/;
+    if(equis.value.match(coordenada) || equis.value <= 10){
+        return true;
+    }else{
+        equis.value = "";
+        alert("Aquí debes ingresar un número entre 1 y 10");
+        x.focus();
+        return false;
+    }
+}
+
+function validarCoordenadaY(yehyeh){
+    var yehyeh = /^[0-9]$/;
+    if(yehyeh.value.match(guai) && yehyeh.value <= 6){
+        return true;
+    }else{
+        yehyeh.value = "";
+        alert("Aquí debes ingresar un número entre 1 y 6");
+        yehyeh.focus();
+        return false;
+    }
+}
+
 //luego creo una clase Auto que tenga dos atributos (posición x e y)
 class Auto { 
 
@@ -38,22 +77,63 @@ class Auto {
     }
 }
 
+
+
+var equis = document.getElementById("x").value;
+var yehyeh = document.getElementById("y").value;
+var car = new Auto(posicion_x, posicion_y); //creo instancia 
+
+/*document.addEventListener("keydown", function (e) {
+ 
+        if (=== 37) {
+       ;
+        }
+        if (=== 38) {
+           ;
+        }
+        if (=== 39) {
+           ;
+        }
+        if ( === 40) {
+            ;
+        }
+    }, false);*/
+
+/*document.addEventListener('keydown', function(e) {
+    }, false);
+    left arrow  37  up arrow  38
+    right arrow 39  down arrow*/
+
 //Ejemplo de tablero enviado por Blanca
 
 function imprimirTablero() {
     //Defino el tablero
     var tablero = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        [ , , , , , , , , , ],
+        [ , , , , , , , , , ],
+        [ , , , , , , , , , ],
+        [ , , , , , , , , , ],
+        [ , , , , , , , , , ],
+        [ , , , , , , , , , ],
     ];
-   
+
+//mono auto
+//coordenadas usuario   
 
 //Obtengo el div en donde se dibujará el tablero
 var divTablero = document.getElementById("tablero");
+
+//coordenadas que pondrá el usuario
+tablero [x][y] = "autito.src"; //image.src
+var autito  = document.createElement("img");
+autito.src = "assets/img/car.png";
+var papaAutito = document.getElementById("tablero");
+
+papaAutito.appendChild(autito);
+
+/* set attr src
+autito.src =
+var divCont = document.createElement("div"); */
 
 //Variables temporales
 var fila, casilla;
